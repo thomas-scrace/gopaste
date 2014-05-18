a5824430381860e7b51524259070a5c38f93a492
Tom Scrace <tom@scrace.org>
2014-05-18 13:30:28 +0100

    More tests for utils.go

--- utils_test.go
+++ utils_test.go
@@ -45,3 +45,42 @@ func TestIsValidStorePathFailsOnNonWriteablePath(t *testing.T) {
 		t.Errorf("isValidStorePath returned no error for non-writeable path")
 	}
 }
+
+func TestIsValidStorePathReturnsNoErrorForValidPath(t *testing.T) {
+	dir, mkErr := makeTestDir(0777)
+	if mkErr != nil {
+		t.Errorf(mkErr.Error())
+	}
+	defer os.RemoveAll(dir)
+
+	if err := isValidStorePath(dir); err != nil {
+		t.Errorf("isValidStorePath returns error for valid path.")
+	}
+}
+
+func TestSafeUint16FromIntReturnsCorrectResultForValidInts(t *testing.T) {
+	var i int
+	for i = 0; i <= 65535; i++ {
+		ui, err := safeUint16FromInt(i)
+		if ui != uint16(i) {
+			t.Errorf("safeUint16FromInt did not return correct result.")
+		}
+		if err != nil {
+			t.Errorf("safeUint16FromInt returned error for valid input.")
+		}
+	}
+}
+
+func TestSafeUint16FromIntReturnsErrorForNegativeInts(t *testing.T) {
+	_, err := safeUint16FromInt(-1)
+	if err == nil {
+		t.Errorf("safeUint16FromInt did not return error for negative int")
+	}
+}
+
+func TestSafeUint16FromIntReturnsErrorForTooLargeInts(t *testing.T) {
+	_, err := safeUint16FromInt(65536)
+	if err == nil {
+		t.Errorf("safeUint16FromInt did not return error for too large int")
+	}
+}
